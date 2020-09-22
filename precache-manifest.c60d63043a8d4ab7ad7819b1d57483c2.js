8874caab04dd8d6cfb4f2e99d49087e3affd8dd8
AmrAlsyed <amralsyed3@gmail.com>
2020-09-22 13:36:03 +0200

    Updates

--- precache-manifest.c60d63043a8d4ab7ad7819b1d57483c2.js
+++ precache-manifest.c60d63043a8d4ab7ad7819b1d57483c2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "80b06a6f29be429325ad431dae781fb9",
+    "revision": "e5af13b1d4b844dd70b17e0a02ad4496",
     "url": "/React_Shopping_Cart/index.html"
   },
   {
-    "revision": "2d73206881c4047ba5a9",
+    "revision": "5ff310d6a0ab97a789a5",
     "url": "/React_Shopping_Cart/static/css/main.e1f47112.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React_Shopping_Cart/static/js/2.93bc6b12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d73206881c4047ba5a9",
-    "url": "/React_Shopping_Cart/static/js/main.025de130.chunk.js"
+    "revision": "5ff310d6a0ab97a789a5",
+    "url": "/React_Shopping_Cart/static/js/main.a3cdd820.chunk.js"
   },
   {
     "revision": "c97b648aec4db00f31b7",
